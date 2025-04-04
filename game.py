0bb66b94d7214a07376f796ea9c31450915093f7
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-04-04 23:53:39 +0530

    defn

--- game.py
+++ game.py
@@ -8,3 +8,12 @@ pygame.init()
 pygame.display.set_caption("Snake Game")
 pygame.font.init()
 random.seed()
+
+SPEED = 0.36
+SNAKE_SIZE = 9
+APPLE_SIZE = SNAKE_SIZE     # keeping food and snake size same
+SEPARATION = 10    # separation between two pixels
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+FPS = 25
+KEY = {"UP":1 , "DOWN":2 , "LEFT":3, "RIGHT":4}
