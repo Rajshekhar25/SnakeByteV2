f39dafe94177748acc7f30acf04b84d9d42a976b
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-04-05 17:08:12 +0530

    resources

--- game.py
+++ game.py
@@ -24,3 +24,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),pygame.HWSURFACE)
 # used hw surface which stands for hardware surface refers to using memory on the video card for storing
 # draws as opposed to main memory
 
+
+# Resources
+score_font = pygame.font.Font(None,38)
+score_numb_font = pygame.font.Font(None,28)
+
